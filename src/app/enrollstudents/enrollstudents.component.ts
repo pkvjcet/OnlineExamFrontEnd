9afd602044f285e64e495c621abec86f8075139d
pkvjcet <30699341+pkvjcet@users.noreply.github.com>
2019-11-16 19:07:28 +0530

    Update enrollstudents.component.ts

--- src/app/enrollstudents/enrollstudents.component.ts
+++ src/app/enrollstudents/enrollstudents.component.ts
@@ -25,7 +25,21 @@ export class EnrollstudentsComponent implements OnInit {
   constructor(private api:ApiService) {
     console.log("In constructor");
 
-   this.api.getExamNames(this.faculty).subscribe((data)=>{
+  //  this.api.getExamNames(this.faculty).subscribe((data)=>{
+  //     this.examnames1=JSON.parse(JSON.stringify(data));
+      
+  //     for (this.i = 0; this.i < this.examnames1.length; this.i++)
+  //     {
+  //      //console.log(this.examnames1[this.i].name);
+  //      this.examnames.push(this.examnames1[this.i].name);
+  //     }
+  //     console.log(this.examnames);
+  //  });
+  }
+
+  ngOnInit() {
+
+    this.api.getExamNames(this.faculty).subscribe((data)=>{
       this.examnames1=JSON.parse(JSON.stringify(data));
       
       for (this.i = 0; this.i < this.examnames1.length; this.i++)
@@ -35,28 +49,68 @@ export class EnrollstudentsComponent implements OnInit {
       }
       console.log(this.examnames);
    });
-  }
 
-  ngOnInit() {
     this.api.getStudents().subscribe((data)=>{
       this.students=JSON.parse(JSON.stringify(data));
       //console.log(this.students.userType)
       //this.dummystudents=JSON.parse(JSON.stringify(data));
-    
-
     });  
+
   }
   onChange(deviceValue) {
+    this.enrolledparticipants=[];
+    this.students=[];
     if((this.examnameSelected=="")||(this.examnameSelected=="Select Test")){
-      window.alert("Select Exam...")
+      window.alert("Select Exam...");
+      this.flag=false;
     }
     else{
+
+      this.api.getStudents().subscribe((data)=>{
+        this.students=JSON.parse(JSON.stringify(data));
+        //console.log(this.students.userType)
+        //this.dummystudents=JSON.parse(JSON.stringify(data));
+      }); 
+
       this.api.viewresult(this.examnameSelected,this.faculty).subscribe((data)=>{
         console.log(JSON.parse(JSON.stringify(data[0].participants)));
        this.enrolledparticipants=JSON.parse(JSON.stringify(data[0].participants));
+
+       var i=0,j=0,flag=0;
+     
+    
+        
+       //window.alert("Loading......." )
+        for(i=0;i<this.enrolledparticipants.length;i++){
+         //  if(alreadyenrolled==true){
+         //    break;
+         //  }
+           for(j=0;j<this.students.length;j++){
+             if((this.enrolledparticipants[i].username==this.students[j].userName)&&(this.enrolledparticipants[i].studentname==this.students[j].userFullName)){
+                 // //alreadyenrolled=true;
+                 // window.alert("already Enrolled..");
+                 // break;
+
+
+                 this.index = this.students.findIndex(x => x.userName ===this.students[j].userName);
+                 this.students.splice(this.index,1);
+
+             }
+           }
+          
+        }
+
        this.flag=true;
     //window.alert(this.participants[0].username);
     });
+   
+
+    //while(this.flag==false);
+   
+
+
+
+
     }
     //console.log(deviceValue);
 }
@@ -72,35 +126,17 @@ export class EnrollstudentsComponent implements OnInit {
     }
     else{
 
-        var i=0,j=0,flag=0;
-     
-    
         
-        //window.alert("Loading......." )
-         for(i=0;i<this.enrolledparticipants.length;i++){
-           if(alreadyenrolled==true){
-             break;
-           }
-            for(j=0;j<this.students.length;j++){
-              if((this.enrolledparticipants[i].username==this.students[j].userName)&&(this.enrolledparticipants[i].studentname==this.students[j].userFullName)){
-                  alreadyenrolled=true;
-                  window.alert("already Enrolled..");
-                  break;
-              }
-            }
-           
-         }
-//          if(alreadyenrolled==false){
-//           this.api.enrollStudents(this.examnameSelected,this.faculty,id,fullname).subscribe(()=>{
-//           });
-
-//          }
-         this.api.enrollStudents(this.examnameSelected,this.faculty,id,fullname).subscribe(()=>{
+         if(alreadyenrolled==false){
+          this.api.enrollStudents(this.examnameSelected,this.faculty,id,fullname).subscribe(()=>{
           });
 
+          this.index = this.students.findIndex(x => x.userName ===id);
+          this.students.splice(this.index,1);
+
+         }
           
-      this.index = this.students.findIndex(x => x.userName ===id);
-      this.students.splice(this.index,1);
+      
       
     }
     
